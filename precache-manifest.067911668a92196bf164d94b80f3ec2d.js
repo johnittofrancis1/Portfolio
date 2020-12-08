e0d5aee6ba94878c22b00e99797c589fd5d08f2c
Johnitto Francis <johnittofrancis46@gmail.com>
2020-12-08 16:11:58 +0530

    Updates

---
+++ precache-manifest.067911668a92196bf164d94b80f3ec2d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef857e9af3f282a7d73a06a4c490d8f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b09ac1a9f9af610149da",
+    "url": "/portfolio/static/css/main.35d59c27.chunk.css"
+  },
+  {
+    "revision": "82ae8cca24f5d738c3c3",
+    "url": "/portfolio/static/js/2.2d046fb6.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/portfolio/static/js/2.2d046fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09ac1a9f9af610149da",
+    "url": "/portfolio/static/js/main.d5bced93.chunk.js"
+  },
+  {
+    "revision": "801ccaea03b45932a78c",
+    "url": "/portfolio/static/js/runtime-main.5355a4ca.js"
+  },
+  {
+    "revision": "83b10b7339a15668f92a6ae5dca90f83",
+    "url": "/portfolio/static/media/CMS.83b10b73.png"
+  },
+  {
+    "revision": "d614db3a826a48d148d9cd1df2048d81",
+    "url": "/portfolio/static/media/LP.d614db3a.svg"
+  },
+  {
+    "revision": "3ecfc43fe02bd44e2d6dc61cac0dea7f",
+    "url": "/portfolio/static/media/Monopoly.3ecfc43f.png"
+  },
+  {
+    "revision": "91a9f272c0fc1143ba33c7836912d62b",
+    "url": "/portfolio/static/media/PCI.91a9f272.png"
+  },
+  {
+    "revision": "abfbe2cfea333b3e91d0bcfd836070ac",
+    "url": "/portfolio/static/media/github.abfbe2cf.svg"
+  },
+  {
+    "revision": "ba5c52a0e89579cfc1a2b3a0dad1aceb",
+    "url": "/portfolio/static/media/instagram.ba5c52a0.svg"
+  },
+  {
+    "revision": "ac39fe0e28cc537d2dff461631282aaa",
+    "url": "/portfolio/static/media/linkedin.ac39fe0e.svg"
+  },
+  {
+    "revision": "5726d5ecf45181fca83493f44c7d2897",
+    "url": "/portfolio/static/media/logo.5726d5ec.svg"
+  },
+  {
+    "revision": "a0b4c394279afe38499afa19e9f94ac3",
+    "url": "/portfolio/static/media/passport.a0b4c394.jpg"
+  }
+]);
